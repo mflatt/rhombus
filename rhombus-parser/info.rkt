c95ed2c61b5eecb732cc46d537f79d1e736f5097
Matthew Flatt <mflatt@racket-lang.org>
2025-08-23 16:16:46 -0600

    basic test suite

---
+++ rhombus-parser/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("rhombus-parser-lib"))
+(define implies '("rhombus-parser-lib"))
+
+(define build-deps '("base"
+                     "rhombus"))
+
+(define pkg-desc "Rhombus parser library")
+
+(define license '(Apache-2.0 OR MIT))
