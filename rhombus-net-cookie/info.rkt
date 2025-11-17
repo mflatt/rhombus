e9fdc81946df12207b0f4ea0c8cd5249e018d598
Matthew Flatt <mflatt@racket-lang.org>
2025-11-17 07:36:43 -0700

    add `rhombus-net-cookie` packages

---
+++ rhombus-net-cookie/info.rkt
@@ -0,0 +1,16 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"
+               "rhombus-net-cookie-lib"))
+(define implies '("rhombus-net-cookie-lib"))
+
+(define build-deps
+  '("rhombus"
+    "rhombus-scribble-lib"
+    "racket-test"))
+
+(define pkg-desc "Rhombus library for RFC 6265 cookies")
+
+(define license '(Apache-2.0 OR MIT))
