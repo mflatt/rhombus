30f3963ac8e43a275cb1d10b6f1598eb5f1aaab2
Matthew Flatt <mflatt@racket-lang.org>
2026-04-03 18:32:20 -0600

    add `ffi`

---
+++ rhombus-ffi/info.rkt
@@ -0,0 +1,17 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"
+               "rhombus-ffi-lib"))
+(define implies '("rhombus-ffi-lib"))
+
+(define build-deps
+  '("rhombus"
+    "racket-doc" ; via `hash_lang`
+    "rhombus-scribble-lib"
+    "racket-test"))
+
+(define pkg-desc "Rhombus foreign-function interface")
+
+(define license '(Apache-2.0 OR MIT))
---
+++ rhombus-ffi/rhombus/ffi/scribblings/rhombus-ffi.scrbl
@@ -0,0 +1,14 @@
+#lang rhombus/scribble/manual
+
+@title(~category: #'#{core}){Rhombus FFI: Foreign-Function Interface}
+
+@docmodule(~open, ffi)
+
+The @rhombusmodname(ffi) library enables direct use of foreign libraries that
+ have a C-based API.
+
+@table_of_contents()
+
+@include_section("overview.scrbl")
+@include_section("api.scrbl")
+@include_section("more.scrbl")
